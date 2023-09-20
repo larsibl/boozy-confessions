917abf44101467f6a1e000e7ce27963b7fea1085
larsibl <60549792+larsibl@users.noreply.github.com>
2023-09-20 15:01:42 +0200

    feature(centercard): add category picker

---
+++ src/components/CategoryPicker.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { RadioGroup, RadioItem } from '@skeletonlabs/skeleton';
+
+    const categories = [ 'Truth', 'Dare', 'Would You Rather', 'Never Have I Ever', 'Paranoia Question' ]
+
+    export let categoryValue: number;
+</script>
+
+<RadioGroup>
+    {#each categories as category, index}
+        <RadioItem bind:group={categoryValue} name="justify" value={index}>{category}</RadioItem>
+    {/each}
+</RadioGroup>
--- src/components/CenterCard.svelte
+++ src/components/CenterCard.svelte
@@ -1,3 +1,9 @@
+<script lang="ts">
+    import CategoryPicker from "./CategoryPicker.svelte";
+
+    let categoryValue = 0;
+</script>
+
 <div class="card p-4 bg-white">
-    CENTER
+    <CategoryPicker {categoryValue} />
 </div>
