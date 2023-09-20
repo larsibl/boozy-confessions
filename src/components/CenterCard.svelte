1963a81879353f1e20a92c3654349092bc935c1c
larsibl <60549792+larsibl@users.noreply.github.com>
2023-09-20 15:15:23 +0200

    feature(centercard): add mode toggler

--- src/components/CenterCard.svelte
+++ src/components/CenterCard.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
     import CategoryPicker from "./CategoryPicker.svelte";
+    import ModeToggler from "./ModeToggler.svelte";
 
     let categoryValue = 0;
+    let modeValue = 0;
 </script>
 
-<div class="card p-4 bg-white">
+<div class="card p-4 bg-white flex gap-2">
     <CategoryPicker {categoryValue} />
+    <ModeToggler {modeValue} />
 </div>
---
+++ src/components/ModeToggler.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    const modes = [ 'PG', 'PG-13', 'R' ]
+
+    export let modeValue: number;
+
+    function onClick() {
+        modeValue = (modeValue + 1) % modes.length
+    }
+</script>
+
+<button type="button" class="btn variant-filled" on:click={onClick}>{modes[modeValue]}</button>
