fefcbba41216ba806d876c4aa2f6196cb6e6c5d1
larsibl <60549792+larsibl@users.noreply.github.com>
2023-10-06 12:04:05 +0200

    style(categorypicker): set colors

--- src/components/CategoryPicker.svelte
+++ src/components/CategoryPicker.svelte
@@ -9,10 +9,18 @@
         { label: 'Paranoia Question', endpoint: '/api/paranoia' },
     ];
 
-    export let endpoint: string;
+    export let endpoint = '/v1/truth';
 </script>
 
-<RadioGroup>
+<RadioGroup
+    active="bg-primary-600 text-white"
+    background="bg-primary-200"
+    border="border-2 border-primary-600"
+    display="flex flex-col md:flex-row"
+    hover="hover:variant-soft-primary"
+    regionLabel="text-primary-600"
+    rounded="rounded-lg md:rounded-full"
+>
     {#each categories as category}
         <RadioItem bind:group={endpoint} name="mode" value={category.endpoint}
             >{category.label}</RadioItem
