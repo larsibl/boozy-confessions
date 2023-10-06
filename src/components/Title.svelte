62d421144d1aa2ddac9f8f8aeb6c7c4d1aa45966
larsibl <60549792+larsibl@users.noreply.github.com>
2023-10-06 12:49:41 +0200

    feature(title): add background to confess

--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -1,1 +1,8 @@
-<h1 class="h1">Are you ready to confess?</h1>
+<h1 class="h1">
+    Are you ready to
+    <span
+        class="before:absolute before:bg-tertiary-500 before:-inset-2 before:opacity-75 before:-rotate-2 before:-skew-x-6 before:-skew-y-3 before:translate-y-0.5 before:rounded-md relative inline-block"
+    >
+        <span class="relative text-white">confess?</span>
+    </span>
+</h1>
