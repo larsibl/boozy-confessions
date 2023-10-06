ec4f135f2517fe0ca4a1a1e55605d6383086b4fe
larsibl <60549792+larsibl@users.noreply.github.com>
2023-10-06 13:31:44 +0200

    style(title): add bottom margin

--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -1,4 +1,4 @@
-<h1 class="h1">
+<h1 class="h1 mb-2">
     Are you ready to
     <span
         class="before:absolute before:bg-tertiary-500 before:-inset-2 before:opacity-75 before:-rotate-2 before:-skew-x-6 before:-skew-y-3 before:translate-y-0.5 before:rounded-md relative inline-block"
